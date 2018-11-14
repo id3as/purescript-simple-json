61073d864aa53293ac7907d6e46ecc1f17810f35
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-11-14 14:17:40 +0000

    Some cleanup

--- test/QuickStart.purs
+++ test/QuickStart.purs
@@ -1,6 +1,5 @@
 module Test.Quickstart where
 
-import Debug.Trace
 import Prelude
 
 import Data.Array as Array
@@ -54,69 +53,64 @@ type WithNullable =
 
 main :: Effect Unit
 main = do
-  -- traceM $ (JSON.readJSON testJSON1 :: E MyRecordAlias)
-  -- traceM $ (JSON.readJSON testJSON2 :: E MyRecordAlias)
-  log "A"
   case JSON.readJSON testJSON1 of
     Right (r :: MyRecordAlias) -> do
       assertEqual { expected: r.apple, actual: "Hello"}
       assertEqual { expected: r.banana, actual: [ 1, 2, 3 ] }
     Left e -> do
       assertEqual { expected: "failed", actual: show e }
-  log "B"
   case JSON.readJSON testJSON2 of
     Right (r1 :: MyRecordAlias) -> do
       assertEqual { expected: "failed", actual: show r1 }
     Left e2 -> do
       let errors = Array.fromFoldable e2
       assertEqual { expected: [ErrorAtProperty "apple" (TypeMismatch "binary" "boolean")], actual: errors }
-  log "C"
-  -- let
-  --   myValue =
-  --     { apple: "Hi"
-  --     , banana: [ 1, 2, 3 ]
-  --     } :: MyRecordAlias
+  let
+    myValue =
+      { apple: "Hi"
+      , banana: [ 1, 2, 3 ]
+      } :: MyRecordAlias
 
-  -- log (JSON.writeJSON myValue) -- {"banana":[1,2,3],"apple":"Hi"}
+  log (JSON.writeJSON myValue) -- {"banana":[1,2,3],"apple":"Hi"}
 
-  -- case JSON.readJSON testJSON3 of
-  --   Right (r :: WithMaybe) -> do
-  --     assertEqual { expected: Just true, actual: r.cherry }
-  --   Left e -> do
-  --     assertEqual { expected: "failed", actual: show e }
+  case JSON.readJSON testJSON3 of
+    Right (r :: WithMaybe) -> do
+      assertEqual { expected: Just true, actual: r.cherry }
+    Left e -> do
+      assertEqual { expected: "failed", actual: show e }
 
-  -- case JSON.readJSON testJSON4 of
-  --   Right (r :: WithMaybe) -> do
-  --     assertEqual { expected: Nothing, actual: r.cherry }
-  --   Left e -> do
-  --     assertEqual { expected: "failed", actual: show e }
+  case JSON.readJSON testJSON4 of
+    Right (r :: WithMaybe) -> do
+      assertEqual { expected: Nothing, actual: r.cherry }
+    Left e -> do
+      assertEqual { expected: "failed", actual: show e }
 
-  -- let
-  --   withJust =
-  --     { cherry: Just true
-  --     } :: WithMaybe
-  --   withNothing =
-  --     { cherry: Nothing
-  --     } :: WithMaybe
+  let
+    withJust =
+      { cherry: Just true
+      } :: WithMaybe
+    withNothing =
+      { cherry: Nothing
+      } :: WithMaybe
 
-  -- log (JSON.writeJSON withJust) -- {"cherry":true}
-  -- log (JSON.writeJSON withNothing) -- {}
+  log (JSON.writeJSON withJust) -- {"cherry":true}
+  log (JSON.writeJSON withNothing) -- {}
 
-  -- case JSON.readJSON testJSON3 of
-  --   Right (r :: WithNullable) -> do
-  --     assertEqual { expected: toNullable (Just true), actual: r.cherry }
-  --   Left e -> do
-  --     assertEqual { expected: "failed", actual: show e }
+  case JSON.readJSON testJSON3 of
+    Right (r :: WithNullable) -> do
+      assertEqual { expected: toNullable (Just true), actual: r.cherry }
+    Left e -> do
+      assertEqual { expected: "failed", actual: show e }
 
-  -- case JSON.readJSON testJSON4 of
-  --   Right (r :: WithNullable) -> do
-  --     assertEqual { expected: "failed", actual: show r }
-  --   Left e -> do
-  --     let errors = Array.fromFoldable e
-  --     assertEqual { expected: [ErrorAtProperty "cherry" (TypeMismatch "Nullable Boolean" "Undefined")], actual: errors }
+  case JSON.readJSON testJSON4 of
+    Right (r :: WithNullable) -> do
+      assertEqual { expected: "failed", actual: show r }
+    Left e -> do
+      let errors = Array.fromFoldable e
+      assertEqual { expected: [ErrorAtProperty "cherry" (TypeMismatch "Nullable boolean" "atom")], actual: errors }
 
-  -- let
-  --   withNullable =
-  --     { cherry: toNullable Nothing
-  --     } :: WithNullable
-  -- log (JSON.writeJSON withNullable) -- {"cherry":null}
+  let
+    withNullable =
+      { cherry: toNullable Nothing
+      } :: WithNullable
+  log (JSON.writeJSON withNullable) -- {"cherry":null}
