4b1eabe90a6d6889412df744bde7a35b3ab646b2
Adrian Roe <adrian@id3as.co.uk>
2020-03-05 15:49:06 +0000

    Remove undefined entries from the JSON

--- src/Simple/JSON.erl
+++ src/Simple/JSON.erl
@@ -7,8 +7,25 @@ end.
 
 % This is a lie for sure, but there is no such thing as undefined in JSON, the JS version relies on objects
 % containing properties with undefined values to be omitted, while we would have to recurse here before passing
-% to JSX to remove them or 
+% to JSX to remove them or
 '_undefined'() -> null.
 
-stringifyJSON(J) -> jsx:encode(J).
+stringifyJSON(J) -> jsx:encode(remove_undefined(J)).
 
+remove_undefined(Map) when is_map(Map) ->
+  maps:fold(fun(_Key, undefined, Acc) ->
+                Acc;
+               (Key, Value, Acc) ->
+                maps:put(Key, remove_undefined(Value), Acc)
+            end,
+            #{},
+            Map);
+
+remove_undefined(Tuple) when is_tuple(Tuple) ->
+  list_to_tuple([remove_undefined(Value) || Value <- tuple_to_list(Tuple)]);
+
+remove_undefined(List) when is_list(List) ->
+  [remove_undefined(Item) || Item <- List];
+
+remove_undefined(Value) ->
+  Value.
